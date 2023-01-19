b926b2c318e5ca4233df076a14f75c491818457c
Andrew Cornejo <atcornejo3@gmail.com>
2023-01-19 08:53:50 -0800

    fixed some prettier formatting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import './App.css';
+import Header from './components/Layout/Header/Header';
+import Footer from './components/Layout/Footer/Footer';
+import Main from './components/Main/Main';
 
-function App() {}
+function App() {
+  return (
+    <section className="main">
+      <Header />
+      <Main />
+      <Footer />
+    </section>
+  );
+}
 
 export default App;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,15 @@
-import { useCountries } from "../../hooks/useCountries";
+import { useCountries } from '../../hooks/useCountries';
 
-import CountryCard from "../Country/CountryCard";
+import CountryCard from '../Country/CountryCard';
 
 export default function Main() {
-    const countries = useCountries();
+  const countries = useCountries();
 
-    return (
-        <main>
-            {countries.map((country) => (
-                <CountryCard key={country.name} {...country} />
-            ))}
-
-        </main>
-    );
-            }
+  return (
+    <main>
+      {countries.map((country) => (
+        <CountryCard key={country.name} {...country} />
+      ))}
+    </main>
+  );
+}
