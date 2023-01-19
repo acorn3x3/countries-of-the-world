36fb854091c3092bf2acdb1513b9c36a57a9f647
Andrew Cornejo <atcornejo3@gmail.com>
2023-01-19 08:50:20 -0800

    fleshed out Main.js, moved components folder into src, generated code for CountryCard.js

---
+++ src/components/Country/CountryCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function CountryCard({ name, iso2, iso3, local_name, continent }) {
+  return <div>
+<div className='country-card'>
+  <div className='details'>
+    <div className='name'>{name}</div>
+    <div className='iso2'>{iso2}</div>
+    <div className='iso3'>{iso3}</div>
+    <div className='local_name'>{local_name}</div>
+    <div className='continent'>{continent}</div>
+
+
+
+
+  </div>
+</div>
+
+
+
+
+  </div>;
+}
---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import { useCountries } from "../../hooks/useCountries";
+
+import CountryCard from "../Country/CountryCard";
+
+export default function Main() {
+    const countries = useCountries();
+
+    return (
+        <main>
+            {countries.map((country) => (
+                <CountryCard key={country.name} {...country} />
+            ))}
+
+        </main>
+    );
+            }
