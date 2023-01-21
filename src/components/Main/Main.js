69d0f0a3a85718e291da4069c0e45cd9bbfe24ad
Andrew Cornejo <atcornejo3@gmail.com>
2023-01-20 16:46:36 -0800

    found the error. Search by continent dropdown now works

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,32 @@
+import React from 'react';
 import { useCountries } from '../../hooks/useCountries';
 import CountryCard from '../Country/CountryCard';
 
 export default function Main() {
   const countries = useCountries();
+  const [continent, setContinent] = React.useState('all');
+  const continents = [...new Set(countries.map(({ continent }) => continent))];
+  const filtered = countries.filter(
+    (country) => country.continent === continent || continent === 'all'
+  );
 
   return (
     <main>
+      <div>
+        <select onChange={(e) => setContinent(e.target.value)}>
+          <option value="all">All</option>
+          {continents.map((continent) => (
+            <option key={continent} value={continent}>
+              {continent}
+            </option>
+          ))}
+        </select>
+      </div>
+      <br></br>
+      <br></br>
+
       <div className="card-container">
-        {countries.map((country) => (
+        {filtered.map((country) => (
           <CountryCard key={country.name} {...country} />
         ))}
       </div>
--- src/hooks/useCountries.js
+++ src/hooks/useCountries.js
@@ -11,5 +11,7 @@ export function useCountries() {
     };
     fetchData();
   }, []);
+
+  
   return countries;
 }
